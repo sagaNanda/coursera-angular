508df6740d608e73508cbf086e58967ed2fe5cf7
sagaNanda <65888860+sagaNanda@users.noreply.github.com>
2020-06-02 19:53:48 +0530

    coursera-angular

---
+++ app.js
@@ -0,0 +1,25 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.checkIfTooMuch = function () {
+    var dish = $scope.dishes;
+    if(typeof dish === "undefined" || dish === "" || dish === null){
+      $scope.message = "Please enter data first";
+    }else {
+      var listOfDishes = dish.split(",");
+      var len =  listOfDishes.length;
+       if(len <=3){
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too much!";
+      }
+  }
+  };
+}
+
+})();
